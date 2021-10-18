23eba85bc88acd4753920c7f1f4dedac4feb52ee
bruno.gracia <bruno.gracia@b2wdigital.com>
2021-10-18 11:32:14 -0300

    teste vuejs

---
+++ precache-manifest.8ef81d8c5cd2cdd59ac9b84a2678e8f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeebcc3aa7fb80876a91",
+    "url": "/css/app.4d22a1bf.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a1d03dd4325ec6ac9d67979bb30ad634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8364bcd691e66aefa7f3",
+    "url": "/js/about.7e4d7ebd.js"
+  },
+  {
+    "revision": "eeebcc3aa7fb80876a91",
+    "url": "/js/app.397737c7.js"
+  },
+  {
+    "revision": "da9c1264f2adf2864b1c",
+    "url": "/js/chunk-vendors.18118d6b.js"
+  },
+  {
+    "revision": "24aa53460ec37db6c92bfefed673e8ff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
